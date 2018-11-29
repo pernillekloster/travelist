61b1a96433a48a96aecf2071667792375adbf3e2
Pernille Kloster <kloster.pernille@gmail.com>
2018-11-29 11:56:35 +0100

    add and get destination works

--- server/routes/trip-create.js
+++ server/routes/trip-create.js
@@ -6,6 +6,56 @@ const Trip = require("../models/Trip")
 const Tip = require("../models/Tip")
 
 
+router.use((req, res, next) => {
+  console.log('DEBUG routes/trips');
+  next()
+})
+
+// Route to get all trips
+router.get('/', (req, res, next) => {
+  Trip.find()
+    .then(trip => {
+      res.json(trip);
+    })
+    .catch(err => next(err))
+});
+
+// Route to add a Trip
+router.post('/', (req, res, next) => {
+  let { destination } = req.body
+  Trip.create({ destination })
+    .then(trip => {
+      res.json({
+        success: true,
+        trip
+      });
+    })
+    .catch(err => next(err))
+});
+
+//GET route to get all trips
+// router.get('/', (req, res, next) => {
+//   console.log('hello from router.get');
+
+// //POST create a trip in home page
+// router.post('/', (req, res, next) => {
+//   let {destination} = req.body
+//   console.log("destination : ", req.body)
+//   Trip.create({destination})
+//   console.log("destination2 : ", req.body)
+
+//   .then(trip => {
+//   console.log("destination3 : ", req.body)
+
+//     res.json({
+//       sucess: true,
+//       trip
+//     })
+//   })
+//   .catch(err => next(err))
+// })
+
+
 // router.get('/secret', isLoggedIn, (req, res, next) => {
 //   res.json({
 //     secret: 42,
