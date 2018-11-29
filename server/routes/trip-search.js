6d07fcf2c7764e4c4c1b1ed2e0faaaaf6b336cd7
Katrin Abel <katrin@abel-essen.de>
2018-11-29 12:05:54 +0100

    update routes trip-search

--- server/routes/trip-search.js
+++ server/routes/trip-search.js
@@ -6,11 +6,38 @@ const Trip = require("../models/Trip")
 const Tip = require("../models/Tip")
 
 
-// router.get('/secret', isLoggedIn, (req, res, next) => {
-//   res.json({
-//     secret: 42,
-//     user: req.user
-//   });
-// });
+router.get('/id/friendtrips', isLoggedIn, (req, res, next) => {
+  // Get id of users trip
+  let id = this.props.match.params.id
+
+  // Find Users trip to get info about destination and friends of user
+  Trip.findById(id)
+  .populate("_creator")
+  .then(tripData => {
+
+    let tripDestination = tripData.destination
+    // let friends = tripData._creator._friends - returns array / in trip match with _creator
+
+      Trip.find({destination: tripDestination})
+      .then(tripsData => {
+        res.json({
+          tripsData
+        })
+      })
+  })
+});
+
+router.get("/i", isLoggedIn, (req, res, next) => {
+  // Get id of friends trip
+  let id = this.props.match.params.id
+
+  // Find selected friends trip and display details
+  Trip.findById(id)
+  .then(tripData => 
+    res.json({
+      tripData
+    })
+  )
+})
 
 module.exports = router;
