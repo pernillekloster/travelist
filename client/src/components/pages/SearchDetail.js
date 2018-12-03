4163ea75bb4bde5d6805d7189b18c51dea331322
Katrin Abel <katrin@abel-essen.de>
2018-12-03 09:24:24 +0100

    update SearchDetails

--- client/src/components/pages/SearchDetail.js
+++ client/src/components/pages/SearchDetail.js
@@ -15,7 +15,6 @@ class SearchDetail extends Component {
     let id = this.props.match.params.id
     let friendTripId = this.props.match.params.friendTripId
 
-    // In BE create copy of tip added and include that one in trip
     api.addTip(id, friendTripId, tipId)
     .then(updateTrip =>
       this.setState({
@@ -25,35 +24,43 @@ class SearchDetail extends Component {
   }
 
   render() {
-    console.log("debug render this.state.selectedTrip", this.state.selectedTrip)
+    console.log(this.state.selectedTrip)
+    const tipCard = []
+
+    this.state.selectedTrip.map(e => {
+    // Sort tips of trips based on category
+    let tips = e.tripData._tip.sort((a,b) => (a.category > b.category ? 1 : -1))
+
+    // Push categories to array
+    for (let i = 0; i < tips.length; i++) {
+    if (i === 0 || tips[i].category !== tips[i-1].category) {
+        tipCard.push(
+        <p>{tips[i].category}</p>
+        )}
+    // Push each tip to array
+    tipCard.push(
+          <div  key={tips[i]._id}> 
+                      <ul>
+                      <li>{tips[i].title}</li>
+                      <li>{tips[i].description}</li>
+                      <li>{tips[i].location}</li>
+                      <button onClick={() => this.handleAdd(tips[i]._id)}>Add</button>
+                      </ul>
+          </div>
+      )
+    }
+    })
+
     return (
       <div className="SearchDetail">
 
-        <h2>Heres your friends tips and recos</h2>
+        <h2>Here are your friends' tips and recos</h2>
 
-        {/* Take the selected trip from the friend and display the included tips  */}
-        {/* Change Tip Model to include isAdded status!! Otherwise all tips will disappear once added */}
+        {/* Take the selected trip from the friend and display the included tips */}
         <div>
         {this.state.isAdded && <div className="Btn-Tip-Added">Gone</div>}
-        
-        {!this.state.isAdded && 
-          this.state.selectedTrip.map(e => 
-      
-              e.tripData._tip.map(t =>
-           
-              <div  key={t._id}> 
-                  <p>{t.title}</p>
-                  <ul>
-                  <li>{t.category}</li>
-                  <li>{t.description}</li>
-                  <li>{t.location}</li>
-                  <button onClick={() => this.handleAdd(t._id)}>Add</button>
-                  </ul>
-                  <br/>
-              </div>
-              )
-          )
-       }
+
+        {!this.state.isAdded && <div>{tipCard}</div>}
         </div>
 
       </div>
--- server/routes/trip-search.js
+++ server/routes/trip-search.js
@@ -64,34 +64,44 @@ router.post("/:id/:friendsId/:newTipId", isLoggedIn, (req, res, next) => {
   let friendsId = req.params.friendsId
   let newTipId = req.params.newTipId
 
-  // Tip.findById(newTipId)
-  // .then(tipData => {
-  //   console.log("debug tipData", tipData)
-  //   Tip.create(tipData)
-  // })
+  // Create copy of tip based on newTipId and pass on that TipId to be added to array
 
-  // Create copy of tip based on newTipId and pass on that TipId to be added to array!
+  Tip.findById(newTipId)
+  .then(tipData => {
+    console.log("debug tipData", tipData)
+    const {category, description, title, location} = tipData
+    const _creator = req.user._id
+    const _trip = id
 
-  // Find Trip of user first to have access to current tip array
-  Trip.findById(id)
+    // Create duplicate of added tip
+    let p1 = Tip.create({_creator, _trip, category, description, title, location})
+    // Find users trip to add duplicate tip
+    let p2 = Trip.findById(id)
 
-  .then(tripData => {
-    
-    // Create new tip array with existing tips and new one
-    let tipArray = tripData._tip
-    let newTipArray = [...tipArray, newTipId]
+    Promise.all([p1, p2])
+    .then(values => {
+      let newTipData = values[0]
+      let tripData = values[1]
 
-    // Find Trip and update tips with new tip
-      Trip.findByIdAndUpdate(id, {
+      // Create new tip array with existing tips and new one
+      let duplicateNewTip = newTipData._id
+      let tipArray = tripData._tip
+      let newTipArray = [...tipArray, duplicateNewTip]
+      console.log("debug duplicate tip", duplicateNewTip)
+      console.log("debug add tip new array", newTipArray)
+
+       // Find Trip and update tips with new tip
+       Trip.findByIdAndUpdate(id, {
         _tip: newTipArray,
-      })
-      .then(updateTrip => {
+        })
+        .then(updateTrip => {
         console.log("debug update trip", updateTrip)
         res.json({
           success: true,
           updateTrip
         })
       })
+    })
   })
 
 })
