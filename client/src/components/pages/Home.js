4951efad4d5e29399b802940fb6b16f3e363c972
Pernille Kloster <kloster.pernille@gmail.com>
2018-12-04 20:35:06 +0100

    hello

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -30,7 +30,7 @@ class Home extends Component {
     this.setState(newState)
   }
 
-  addTrip=(e)=> {
+  addTrip = (e) => {
     e.preventDefault()
     let data = {
       destination: this.state.destination
@@ -54,7 +54,7 @@ class Home extends Component {
 
   getTripColor(trip) {
     let colors = ['#1F5B66', '#257888', '#6E9FA8']
-    let colorIndex = parseInt("0x"+trip._id.substr(-10)) % colors.length
+    let colorIndex = parseInt("0x" + trip._id.substr(-10)) % colors.length
     return colors[colorIndex]
   }
 
@@ -70,23 +70,28 @@ class Home extends Component {
       .catch(err => console.log(err))
   }
 
-  
-
   render() {
     return (
       <div>
         <h4 className="homeHeader">Where is your next destination?</h4>
+        <div className="destinationbox1" style={{ backgroundColor: "#257888", opacity: 0.3 }}>
+        <Button className="btn btn-add-trip" onClick={this.toggle}>
+          <div className="plusicon">
+            <img src="../../../images/plusSign.png" />
+          </div>
+        </Button>
+        </div>
 
-        <Button className="btn btn-add-trip" onClick={this.toggle}>Add new tip</Button>
+        {/* <Button className="btn btn-add-trip" onClick={this.toggle}>Add new tip</Button> */}
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader toggle={this.toggle}>Add your new destination</ModalHeader>
           <form>
-          <ModalBody>
-            Destination: <input type="text" style={{border: 'solid'}} value={this.state.destination} onChange={(e) => this.handleInputChange("destination", e)} /> <br/>
-            <Button color="primary" onClick={this.addTrip}>Save trip</Button>{' '}
-          </ModalBody>
+            <ModalBody>
+              Destination: <input type="text" style={{ border: 'solid' }} value={this.state.destination} onChange={(e) => this.handleInputChange("destination", e)} /> <br />
+              <Button color="primary" onClick={this.addTrip}>Save trip</Button>{' '}
+            </ModalBody>
           </form>
-        </Modal> 
+        </Modal>
 
 
 
@@ -94,19 +99,21 @@ class Home extends Component {
           <input type="text" value={this.state.destination} onChange={(e) => { this.handleInputChange("destination", e) }} />
           <Button onClick={this.addTrip}> Add destination </Button>
         </form>
-        <div className="destinationbox1" style={{backgroundColor: "#257888",opacity: 0.3}}>
-        <NavLink to="/" exact >
-        <div  className="plusicon">
-        <img src="../../../images/plusSign.png"/>
-      </div>
-      </NavLink>
+        <div className="destinationbox1" style={{ backgroundColor: "#257888", opacity: 0.3 }}>
+          <NavLink to="/" exact >
+            <div className="plusicon">
+              <img src="../../../images/plusSign.png" />
+            </div>
+          </NavLink>
         </div>
         <div style={{ margin: 'auto' }}>
-          {this.state.trips.map((t,i) => (
-            <div className="destinationbox" key={i}   style={{backgroundColor: this.getTripColor(t)}}>
+          {this.state.trips.map((t, i) => (
+            <Link to={`trip-detail/${t._id}`}> 
+            <div className="destinationbox" key={i} style={{ backgroundColor: this.getTripColor(t) }}>
               {/* <tr key={t._id}> */}
-              <Link to={`trip-detail/${t._id}`}> <a>{t.destination}</a></Link>
+              <a className="link-to-detailed-trip"style={{color: 'white'}}>{t.destination}</a>
             </div>
+            </Link>
           ))}
         </div>
       </div>
