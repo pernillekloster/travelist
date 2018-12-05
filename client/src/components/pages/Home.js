995a8fcc67cb99178192d43feb9b678a494a163e
Katrin Abel <katrin@abel-essen.de>
2018-12-05 17:14:24 +0100

    styling edit tips

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
-import { Route, NavLink, Switch } from 'react-router-dom';
+import { Button, Modal, ModalHeader, ModalBody, Table } from 'reactstrap';
 import api from '../../api'
-import AddTip from './AddTip'
-import TripDetail from './TripDetail'
+
 
 class Home extends Component {
   constructor(props) {
     super(props);
     this.state = {
       trips: [],
+      allTrips: [],
       modal: false,
-      // tripsColors: [],
       destination: "",
     };
     this.toggle = this.toggle.bind(this);
@@ -47,10 +45,12 @@ class Home extends Component {
       .catch(err => console.log(err))
   }
 
-  // generateRandomColor() {
-  //   let colors = ['#1F5B66', '#257888', '#6E9FA8']
-  //   return colors[Math.floor(colors.length*Math.random())]
-  // }
+  chooseDestination(e, chosenDestination) {
+    e.preventDefault()
+    this.setState({
+      destination: chosenDestination
+    })
+  }
 
   getTripColor(trip) {
     let colors = ['#1F5B66', '#257888', '#6E9FA8']
@@ -59,18 +59,52 @@ class Home extends Component {
   }
 
   componentDidMount() {
+    api.getTrips()
+      .then(allTrips => {
+        console.log("debug frontend addtripsearch", allTrips)
+        this.setState({
+          allTrips: allTrips,
+        })
+      })
     api.getUserTrips()
       .then(trips => {
         this.setState({
           trips: trips,
           destination: "",
-          // tripsColors: trips.map(trip => this.generateRandomColor())
         })
       })
       .catch(err => console.log(err))
   }
 
   render() {
+
+    // const reducedArr =
+    // this.state.allTrips.slice()
+
+    // const newArr = (reducedArr) => {
+    //   for (let i = 0; i < reducedArr.length; i++) {
+    //     if (reducedArr[i].destination !== reducedArr[i+1].destination) {
+    //       newArr.push(reducedArr[i].destination)
+    //     }
+    //   }
+    //   return newArr
+    // }
+
+    // console.log("KATRIN", newArr)    
+    // const newNewArr = reducedArr.filter((v,i) => reducedArr.indexOf(v.destination) === i)
+    // console.log("debug search", newNewArr)
+
+    const  filteredArr = 
+    this.state.allTrips
+      .filter(t => 
+          t.destination
+            .toLowerCase()
+            .includes(this.state.destination.toLocaleLowerCase())
+        )
+      .sort((a,b) => (a.destination > b.destination ? 1 : -1))
+
+  
+
     return (
       <div>
       <h4 className="homeHeader">Where is your next destination?</h4>
@@ -85,22 +119,34 @@ class Home extends Component {
         </div>
       </div>
 
-        {/* <Button className="btn btn-add-trip" onClick={this.toggle}>Add new tip</Button> */}
+        
         <Modal isOpen={this.state.modal} toggle={this.toggle} className={this.props.className}>
           <ModalHeader className="important-header modalHeader" toggle={this.toggle}>Add your new destination</ModalHeader>
           <form>
             <ModalBody className="detail-size modalBody">
-          <div className="inputCategoryTrip">
-
-            <input className="inputAddTip" type="text" placeholder="Destination" style={{ border: 'solid' }} value={this.state.destination} onChange={(e) => this.handleInputChange("destination", e)} /> <br />
+            
+            <div className="inputCategoryTrip">
+              <input className="inputAddTip" type="text" placeholder="Destination" style={{ border: 'solid' }} value={this.state.destination} onChange={(e) => this.handleInputChange("destination", e)} /> <br />
             </div>
+
+            <Table>
+            <tbody>
+              {filteredArr
+              .map(t => (
+                <tr className="create-trip-search" key={"table"-t._id}>
+                  <button className="create-trip-search"  onClick={(e) => this.chooseDestination(e, t.destination)}>{t.destination}</button>
+                </tr>
+              ))}
+             </tbody>
+            </Table>
+
               <Button className="btn btn-trip-detail-saveTip" color="#1F5B66" onClick={this.addTrip}>Save trip</Button>{' '}
             </ModalBody>
           </form>
         </Modal>
 
 
-        {/* <div className="homeboxes" style={{ margin: 'auto' }}> */}
+        
           {this.state.trips.map((t, i) => (
             <Link to={`trip-detail/${t._id}`}> 
             <div className="destinationbox" key={i} style={{ backgroundColor: this.getTripColor(t) }}>
@@ -108,7 +154,7 @@ class Home extends Component {
             </div>
             </Link>
           ))}
-        {/* </div> */}
+      
 
       </div>
       </div>
