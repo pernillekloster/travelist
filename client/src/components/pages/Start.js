04780d17cfe8ec7827a9bb0aeb7a343f63599ea3
“Eullin <eullinch@gmail.com>
2018-12-06 16:40:44 +0100

    latest changes pull

--- client/src/components/pages/Start.js
+++ client/src/components/pages/Start.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'reactstrap'
+import "../../styles/Eullin.css";
 
 
 class Start extends Component {
@@ -21,8 +22,8 @@ class Start extends Component {
   render() {
     return (
       <div className="Start">
-        <div className="onboarding important-header">
-            <p className="roboto"> All your travel tips <br/> in one place. </p>
+        <div className="onboarding important-header test">
+        <span>All your <span className="second-word-formatting">travel tips</span> <br/> in one place.</span>
         </div>
 
           <button className="btn btn-trip-detail-search" color="#1F5B66" onClick={(e) => this.handleSignup(e)}>
